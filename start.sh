3cef425e5d858850a8727f88117353776f7ddfc2
João José Oliveira Leal <joaojose@MacBook-Air-de-Joao.local>
2019-09-15 21:23:27 -0300

    Ajuste no script start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ cd AnaliseCreditoBackend
 mvn clean install -DskipTests
 docker build -t joaojoseleal/analise-credito-backend .
 
+cd ../AnaliseCreditoFrontend
 echo "----> BUILD FRONTEND <----"
 docker build -t joaojoseleal/analise-credito-frontend .
 
